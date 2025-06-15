bcd53a8d97e85c7fdb3f67eabbff7eabae0bc6e4
obi1kanobii <roee@betterchoice.live>
2025-06-15 19:24:29 +0300

    userss

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -43,12 +43,15 @@ export default function Chat() {
     try {
       let userData;
 try {
-  userData = await User.me();
-} catch (err) {
-  // fallback for Netlify
-  const res = await fetch('/data.json');
-  userData = await res.json();
-}
+  try {
+    userData = await User.me();
+  } catch (err) {
+    // fallback for Netlify
+    const res = await fetch('/client.json');
+    userData = await res.json();
+    userData.selectedClientId = userData.id;  // 👈 simulate selected client
+  }
+  
 
       
       if (!userData.selectedClientId) {
